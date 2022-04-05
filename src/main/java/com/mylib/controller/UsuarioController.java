44b415f1c7325da38dce192df8bf24afb49f5e8a
Gabriel <gabriel.palermo@unosolucoes.com.br>
2022-04-05 14:45:09 -0300

    Criação dos métodos de usuário

--- src/main/java/com/mylib/controller/UsuarioController.java
+++ src/main/java/com/mylib/controller/UsuarioController.java
@@ -2,20 +2,24 @@ package com.mylib.controller;
 
 import com.mylib.model.Usuario;
 import com.mylib.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @GetMapping
     public ResponseEntity<?> listar() {
@@ -23,4 +27,18 @@ public class UsuarioController {
         return !usuarios.isEmpty() ? ResponseEntity.ok(usuarios) : ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPelaId(@PathVariable Integer id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.isPresent() ? ResponseEntity.ok(usuario) : ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario, HttpServletResponse response) {
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(usuarioSalvo.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(usuarioSalvo);
+    }
 }
