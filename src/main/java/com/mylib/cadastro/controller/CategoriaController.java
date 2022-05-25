311c4db06c036e9150e36bbf7cb4addafbd1af25
GabeHenrique <gabriel.palermo@unosolucoes.com.br>
2022-05-25 17:38:42 -0300

    Adicionar o método de Delete

--- src/main/java/com/mylib/cadastro/controller/CategoriaController.java
+++ src/main/java/com/mylib/cadastro/controller/CategoriaController.java
@@ -2,6 +2,7 @@ package com.mylib.cadastro.controller;
 
 import com.mylib.cadastro.model.Categoria;
 import com.mylib.cadastro.repository.CategoriaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,26 +12,31 @@ import java.util.Optional;
 @RequestMapping("/categorias")
 public class CategoriaController {
 
-    private final CategoriaRepository categoriaRepository;
+    private final CategoriaRepository repository;
 
     public CategoriaController(CategoriaRepository categoriaRepository) {
-        this.categoriaRepository = categoriaRepository;
+        this.repository = categoriaRepository;
     }
 
     @GetMapping
     public List<?> listar() {
-        return categoriaRepository.findAll();
+        return repository.findAll();
     }
 
     @GetMapping("/{id}")
     public Optional<?> buscarPelaId(@PathVariable Integer id) {
-        return categoriaRepository.findById(id);
+        return repository.findById(id);
     }
 
     @PostMapping
     public void criar(@RequestBody Categoria categoria) {
-        categoriaRepository.save(categoria);
+        repository.save(categoria);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
 
 }
--- src/main/java/com/mylib/cadastro/controller/ProdutoController.java
+++ src/main/java/com/mylib/cadastro/controller/ProdutoController.java
@@ -1,9 +1,13 @@
 package com.mylib.cadastro.controller;
 
 import com.mylib.cadastro.model.Produto;
+import com.mylib.cadastro.model.Usuario;
 import com.mylib.cadastro.repository.ProdutoRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,26 +15,31 @@ import java.util.Optional;
 @RequestMapping("/produto")
 public class ProdutoController {
 
-    private final ProdutoRepository produtoRepository;
+    private final ProdutoRepository repository;
 
     public ProdutoController(ProdutoRepository produtoRepository) {
-        this.produtoRepository = produtoRepository;
+        this.repository = produtoRepository;
     }
 
     @GetMapping
     public List<?> listar() {
-        return produtoRepository.findAll();
+        return repository.findAll();
     }
 
     @GetMapping("/{id}")
     public Optional<?> buscarPelaId(@PathVariable Integer id) {
-        return produtoRepository.findById(id);
+        return repository.findById(id);
     }
 
     @PostMapping
     public void criar(@RequestBody Produto produto) {
-        produtoRepository.save(produto);
+        repository.save(produto);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
 
 }
