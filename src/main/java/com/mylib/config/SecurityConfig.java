915bc561426bb0f497763dfc2765337467550510
GabeHenrique <gabriel.palermo@unosolucoes.com.br>
2022-05-27 18:06:33 -0300

    -

--- src/main/java/com/mylib/config/SecurityConfig.java
+++ src/main/java/com/mylib/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/usuario/create", "/usuario/create/**").permitAll()
+                .authorizeRequests().antMatchers("/").permitAll()
                 .and().httpBasic();
     }
 }
