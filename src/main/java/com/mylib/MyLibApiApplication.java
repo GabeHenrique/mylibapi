66c1b0b4d1047462ce1b80ae72303028920c752b
GabeHenrique <gabriel.palermo@unosolucoes.com.br>
2022-05-27 19:33:42 -0300

    Implementação do login simples

--- src/main/java/com/mylib/MyLibApiApplication.java
+++ src/main/java/com/mylib/MyLibApiApplication.java
@@ -2,8 +2,11 @@ package com.mylib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Controller
@@ -27,4 +30,16 @@ public class MyLibApiApplication {
         public String login() {
             return "login";
         }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            }
+        };
+    }
 }
