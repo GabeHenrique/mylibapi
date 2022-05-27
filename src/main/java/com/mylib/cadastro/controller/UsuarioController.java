cc3929243be9b2854879a2cfa1e3284ed5d63a9a
GabeHenrique <gabriel.palermo@unosolucoes.com.br>
2022-05-27 08:56:18 -0300

    Implementação do login simples

--- src/main/java/com/mylib/cadastro/controller/UsuarioController.java
+++ src/main/java/com/mylib/cadastro/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
